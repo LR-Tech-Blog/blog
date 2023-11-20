6409ddcc0ea17e3db75244eb6ea1ea997d7e4c49
LuanRoger <luan.roger.2003@gmail.com>
2023-11-20 19:56:09 -0300

    feat: create 404 custom page

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -6,3 +6,7 @@ export function getGravatarImageUrl(email: string, size: number): string {
     const hash = sha256(email)
     return `${gravatarBaseUrl}${hash}?s=${size}`
 }
+
+export function generateRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
