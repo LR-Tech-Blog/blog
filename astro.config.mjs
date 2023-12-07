7f0081978372c4a195397d216fc86468aa5481d8
LuanRoger <luan.roger.2003@gmail.com>
2023-12-07 08:58:10 -0300

    feat: add the Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "http://localhost:4321"
+  site: "http://localhost:4321",
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    speedInsights: {
+      enabled: true
+    }
+  })
 });
