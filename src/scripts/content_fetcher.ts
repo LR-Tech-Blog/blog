9ea87674c09e1b30e254f819615efa853146778a
LuanRoger <luan.roger.2003@gmail.com>
2023-12-09 10:08:43 -0300

    feat: sort posts by date

--- src/scripts/content_fetcher.ts
+++ src/scripts/content_fetcher.ts
@@ -3,7 +3,8 @@ import { type CollectionEntry, getCollection } from 'astro:content';
 export async function getPosts(): Promise<CollectionEntry<'posts'>[]> {
     const posts = await getCollection("posts", ({ data }) => {
         return data.draft === false
-    });
+    })
+    posts.sort((p1, p2) => p2.data.date.valueOf() - p1.data.date.valueOf())
 
     return posts
 }
