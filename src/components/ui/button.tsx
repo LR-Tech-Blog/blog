e5ff683eb4e717321eb1372430970bc6680a96fc
ROG <luan.roger.2003@gmail.com>
2024-04-07 17:09:00 -0300

    feat: add `pagination`, `separator` and `tooltip` components from shadcn

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,10 +20,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-10 px-1 py-1",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "transition hover:scale-110 h-10 w-10",
       },
     },
     defaultVariants: {
