d7ce946707755b399bb1ea1d2e4673348d9690cb
ROG <luan.roger.2003@gmail.com>
2024-04-07 09:01:08 -0300

    feat: add shadcn-ui and remove daisy-ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import getReadTime from './src/scripts/reading_time';
-
 import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon({
     include: {
       simple_icons: ["*"]
     }
-  })],
+  }), react()],
   site: "https://lrtechblog.vercel.app",
   output: "hybrid",
   adapter: vercel({
