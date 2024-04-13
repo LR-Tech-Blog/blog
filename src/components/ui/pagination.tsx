73f62ed2f681b8760feee50a1d7365364fa538ac
ROG <luan.roger.2003@gmail.com>
2024-04-13 16:33:31 -0300

    feat: create post fetch from page and component

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -21,7 +21,7 @@ const PaginationContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <ul
     ref={ref}
-    className={cn("flex flex-row items-center gap-1", className)}
+    className={cn("flex flex-row items-center gap-2", className)}
     {...props}
   />
 ))
@@ -67,11 +67,10 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 p-3", className)}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -83,10 +82,9 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 p-3", className)}
     {...props}
   >
-    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 )
--- src/scripts/content_fetcher.ts
+++ src/scripts/content_fetcher.ts
@@ -1,24 +1,58 @@
-import { type CollectionEntry, getCollection } from 'astro:content';
+import { type CollectionEntry, getCollection } from "astro:content";
+import { POSTS_PER_PAGE } from "./consts";
 
-export async function getPosts(): Promise<CollectionEntry<'posts'>[]> {
-    const posts = await getCollection("posts", ({ data }) => {
-        return data.draft === false
-    })
-    posts.sort((p1, p2) => p2.data.date.valueOf() - p1.data.date.valueOf())
+export type FilterCallback = (post: CollectionEntry<"posts">) => boolean;
+const defaultFilter = function (entry: CollectionEntry<"posts">) {
+  return entry.data.draft === false;
+};
 
-    return posts
+export async function getPosts(
+  filter?: FilterCallback
+): Promise<CollectionEntry<"posts">[]> {
+  const posts = await getCollection("posts", filter || defaultFilter);
+  posts.sort((p1, p2) => p2.data.date.valueOf() - p1.data.date.valueOf());
+
+  return posts;
 }
 
-export async function getPostsByType(postType: string): Promise<CollectionEntry<'posts'>[]> {
-    const posts = await getPosts()
-    return posts.filter(post => post.data.postType === postType)
+export async function getPostsByType(
+  postType: string
+): Promise<CollectionEntry<"posts">[]> {
+  const posts = await getPosts();
+  return posts.filter((post) => post.data.postType === postType);
 }
 
-export async function getPostsWithTag(tag: string): Promise<CollectionEntry<'posts'>[]> {
-    if(tag.trim().length == 0) {
-        return []
+export async function getPostsWithTag(
+  tag: string
+): Promise<CollectionEntry<"posts">[]> {
+  if (tag.trim().length == 0) {
+    return [];
+  }
+
+  const posts = await getPosts();
+  return posts.filter((post) => post.data.tags.includes(tag));
+}
+
+export async function getPostsFromPage(
+  page: number
+): Promise<CollectionEntry<"posts">[]> {
+  const skip = (page - 1) * POSTS_PER_PAGE;
+  const firstPage = skip === 0
+  let postsSkiped = 0
+  let postsTaked = 0
+
+  const pagePosts = await getPosts(({ data }) => {
+    if(data.draft || (!firstPage && postsSkiped <= skip)) {
+      postsSkiped++
+      return false
     }
 
-    const posts = await getPosts()
-    return posts.filter(post => post.data.tags.includes(tag))
+    if(postsTaked < POSTS_PER_PAGE) {
+      return true
+    }
+
+    return false
+  });
+
+  return pagePosts.slice(0, POSTS_PER_PAGE);
 }
