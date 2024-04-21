78c8180866d8bd43c578730ebddf14b71646acd3
ROG <luan.roger.2003@gmail.com>
2024-04-21 15:19:39 -0300

    feat: create more social share buttons
    - Reddit
    - Telegram
    - WhatsApp

--- src/scripts/share.ts
+++ src/scripts/share.ts
@@ -1,11 +1,26 @@
-const messageText = "Check it out!";
+const messageText = "Olha esse artigo do LR Tech Blog!";
+
+export function shareLinkedin(url: string) {
+    const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
+    window.open(linkedinUrl, "_blank");
+}
 
 export function shareTwitter(url: string) {
     const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${messageText}`;
     window.open(twitterUrl, "_blank");
 }
 
-export function shareLinkedin(url: string) {
-    const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${url}`;
-    window.open(linkedinUrl, "_blank");
+export function shareWhatsApp(url: string) {
+    const whatsappUrl = `https://api.whatsapp.com/send?text=${messageText} ${url}`;
+    window.open(whatsappUrl, "_blank");
+}
+
+export function shareTelegram(url: string) {
+    const telegramUrl = `https://telegram.me/share/url?url=${url}&text=${messageText}`
+    window.open(telegramUrl, "_blank");
+}
+
+export function shareReddit(url: string) {
+    const redditUrl = `https://www.reddit.com/submit?url=${url}&title=${messageText}`;
+    window.open(redditUrl, "_blank");
 }
