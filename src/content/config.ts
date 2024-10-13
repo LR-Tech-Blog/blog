72f522481de1417dfb8e0a9cc767af9d4c2ec5f5
LuanRoger <luan.roger.2003@gmail.com>
2024-10-13 13:48:35 -0300

    feat: chanage content path and schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,52 +1,54 @@
+import { file, glob } from "astro/loaders";
 import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        id: z.string(),
-        title: z.string(),
-        author: reference("authors"),
-        date: z.date(),
-        draft: z.boolean(),
-        tags: z.array(z.string()),
-        postType: z.string(),
-        description: z.string(),
-        image: z.object({
-            src: z.string(),
-            alt: z.string(),
-            href: z.string().url().optional(),
-        })
-    })
-})
+  loader: glob({ pattern: "**/*.mdx", base: "./src/data/posts" }),
+  schema: z.object({
+    title: z.string(),
+    author: reference("authors"),
+    date: z.date(),
+    draft: z.boolean(),
+    tags: z.array(z.string()),
+    postType: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      href: z.string().url().optional(),
+    }),
+  }),
+});
 
 const authorProfile = defineCollection({
-    type: "data",
-    schema: z.object({
+  loader: file("./src/data/authors.json"),
+  schema: z.object({
+    name: z.string(),
+    age: z.number(),
+    email: z.string(),
+    linkedin: z.string().url(),
+    github: z.string().url(),
+    profileImageUrl: z.string().url().nullable(),
+    profileImageAlt: z.string().nullable(),
+    profileImageType: z.enum(["URL", "Gravatar"]),
+    stack: z.array(
+      z.object({
         name: z.string(),
-        age: z.number(),
-        email: z.string(),
-        linkedin: z.string().url(),
-        github: z.string().url(),
-        profileImageUrl: z.string().url().nullable(),
-        profileImageAlt: z.string().nullable(),
-        profileImageType: z.enum(["URL", "Gravatar"]),
-        stack: z.array(z.object({
-            name: z.string(),
-            iconName: z.string(),
-            iconColor: z.string(),
-        })),
-    })
-})
+        iconName: z.string(),
+        iconColor: z.string(),
+      })
+    ),
+  }),
+});
 
 const authorDescription = defineCollection({
-    type: "content",
-    schema: z.object({
-        author: reference("authors")
-    })
-})
+  loader: glob({ pattern: "**/*.mdx", base: "./src/data/authorsDescription" }),
+  schema: z.object({
+    author: reference("authors"),
+  }),
+});
 
 export const collections = {
-    'posts': blogCollection,
-    'authors': authorProfile,
-    'authorsDescription': authorDescription
-}
+  posts: blogCollection,
+  authors: authorProfile,
+  authorsDescription: authorDescription,
+};
